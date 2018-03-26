76e5b0af1d1bd232a565205eb240aee4ca2cde1e
OrangeCandies <liuhui.mail@foxmail.com>
2018-03-26 18:21:13 +0800

    明天阿里视频面试 好好准备！

--- src/anything/sort/Heap.java
+++ src/anything/sort/Heap.java
@@ -4,7 +4,84 @@ import java.util.Random;
 
 public class Heap {
 
-    private int size = 0;
+    private int size;
+    private int capcity;
+    private int[] data;
+
+    public Heap(int capcity) {
+        this.capcity = capcity;
+        this.data = new int[capcity];
+        size = 0;
+    }
+
+    public void insert(int input) {
+        if (size == capcity) {
+            resize();
+        }
+        data[size] = input;
+        shiftUp(size);
+        size++;
+    }
+
+    public int pop(){
+        size--;
+        int target = data[0];
+        data[0] = data[size];
+        data[size] = target;
+        shiftDown(0,size);
+
+        return target;
+    }
+
+    private void shiftDown(int pos, int size) {
+        int l = pos * 2 + 1;
+        int r = pos * 2 + 2;
+        int maxPos = pos;
+
+        if(l < size && data[maxPos] > data[l]){
+            maxPos = l;
+        }
+        if(r < size && data[maxPos] > data[r]){
+            maxPos = r;
+        }
+        if(maxPos != pos){
+            int temp = data[maxPos];
+            data[maxPos] = data[pos];
+            data[pos] = temp;
+
+            shiftDown(maxPos,size);
+        }
+        
+    }
+
+    private void shiftUp(int size) {
+        int father = (size - 1) / 2;
+        while (data[size] < data[father]) {
+            int temp = data[size];
+            data[size] = data[father];
+            data[father] = temp;
+
+            father = size;
+            father = (father - 1) / 2;
+        }
+    }
+
+    private void resize() {
+        capcity = capcity * 2;
+        int[] newData = new int[capcity];
+        for (int i = 0; i < size; i++) {
+            newData[i] = data[i];
+        }
+        this.data = newData;
+    }
+
+    public void heapify(){
+        for(int i = size/2; i >= 0; i --){
+            shiftDown(i,size);
+        }
+    }
+
+  /*  private int size = 0;
     private int maxSize = 0;
     private int[] data;
 
@@ -24,7 +101,6 @@ public class Heap {
     }
 
 
-
     public int pop() {
         if (size == 0) {
             throw new ArrayIndexOutOfBoundsException("size aleady have no element");
@@ -39,7 +115,6 @@ public class Heap {
     }
 
 
-
     private void resize() {
         maxSize = maxSize * 2;
         int[] newData = new int[maxSize];
@@ -50,7 +125,8 @@ public class Heap {
     }
 
 
-    private void shiftUp(int current){
+
+    private void shiftUp(int current) {
         int father = (current - 1) / 2;
         while (data[current] < data[father]) {
             int temp = data[current];
@@ -80,11 +156,12 @@ public class Heap {
             shiftDown(minPos, size);
         }
     }
-    public void heapify(){
-        for(int i = size/2; i >= 0 ; i--){
-            shiftDown(i,size);
+
+    public void heapify() {
+        for (int i = size / 2; i >= 0; i--) {
+            shiftDown(i, size);
         }
-    }
+    }*/
 
     public static void main(String[] args) {
         Heap heap = new Heap(50);
