f03806d62cbfbfb951203c70438b2c8ec310b8e9
OrangeCandies <liuhui.mail@foxmail.com>
2018-04-15 18:23:40 +0800

    完成了Leetcode算法初级篇

--- src/anything/leetcode/easy/Problem43.java
+++ src/anything/leetcode/easy/Problem43.java
@@ -5,11 +5,17 @@ public class Problem43 {
     class Solution {
         // you need treat n as an unsigned value
         public int reverseBits(int n) {
-
-            n <<= 1;
-          //  n ^= 0xffffffff;
-            n >>= 1;
-            return n;
+            int value = 0;
+            for(int i =0; i < 32; i ++){
+                if((n & 1) == 1){
+                    value = (value << 1) + 1;
+                    n >>= 1;
+                }else{
+                    value <<= 1;
+                    n >>= 1;
+                }
+            }
+            return value;
         }
     }
 
