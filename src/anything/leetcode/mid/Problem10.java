ff9752abf065ec780e96e084f6c3a3f5a2ef440e
OrangeCandies <liuhui.mail@foxmail.com>
2018-04-23 08:34:26 +0800

    Day1

--- src/anything/leetcode/mid/Problem10.java
+++ src/anything/leetcode/mid/Problem10.java
@@ -2,13 +2,30 @@ package anything.leetcode.mid;
 
 import anything.TreeNode;
 
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Stack;
 
 public class Problem10 {
     class Solution {
         public List<Integer> inorderTraversal(TreeNode root) {
-            
+            List<Integer> ans = new LinkedList<>();
+            Stack<TreeNode> stack = new Stack<>();
+            TreeNode cur = root;
+            while(!stack.isEmpty() || cur != null){
+                while (cur != null){
+                    stack.push(cur);
+                    cur = cur.left;
+                }
+                if(!stack.isEmpty()){
+                    cur = stack.pop();
+                    ans.add(cur.val);
+                }
+                cur = cur.right;
+            }
+            return ans;
         }
+
     }
 
 
