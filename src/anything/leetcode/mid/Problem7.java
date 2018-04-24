da78b9bc3c3b2010bf3acb00495fb05955085302
OrangeCandies <liuhui.mail@foxmail.com>
2018-04-24 08:34:35 +0800

    Day1

--- src/anything/leetcode/mid/Problem7.java
+++ src/anything/leetcode/mid/Problem7.java
@@ -6,38 +6,19 @@ public class Problem7 {
 
     class Solution {
         public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-            ListNode p1 = l1;
-            ListNode p2 = l2;
-            ListNode ans = new ListNode(0);
-            ListNode head = ans;
+            ListNode ans = new ListNode(1);
+            ListNode cur = ans;
             int flag = 0;
-            while(p1 != null && p2 != null) {
-                ans.next = new ListNode((flag + p1.val + p2.val) % 10);
-                ans = ans.next;
-                flag = (p1.val+p2.val)/10;
-                p1 = p1.next;
-                p2 = p2.next;
+            while(l1 != null || l2 != null || flag != 0){
+                int sum = (l1 != null ? l1.val : 0) +( l2 != null ? l2.val : 0 ) + flag;
+                flag = sum/10;
+                cur.next = new ListNode(sum%10);
+                cur = cur.next;
+                l1 = l1 == null ? l1 :  l1.next;
+                l2 = l2 == null ? l2 : l2.next;
+
             }
-            if(p1 != null){
-                while ( p1.next != null ){
-                    p1.val = (p1.val+flag) %10;
-                    flag = (p1.val + flag) /10;
-                    p1 = p1.next;
-                }
-                if(flag != 0){
-                    p1.next = new ListNode(flag);
-                }
-            }else {
-                while ( p2.next != null ){
-                    p2.val = (p2.val+flag) %10;
-                    flag = (p2.val + flag) /10;
-                    p2 = p2.next;
-                }
-                if(flag != 0){
-                    p2.next = new ListNode(flag);
-                }
-            }
-            return head.next;
+            return ans.next;
         }
     }
 
