c272d1a22b64a7241daf2de2674be2112d42ccf7
LiuHui <35681600+OrangeCandies@users.noreply.github.com>
2018-05-03 20:29:22 +0800

    Update Problem14.java

--- src/anything/leetcode/mid/Problem14.java
+++ src/anything/leetcode/mid/Problem14.java
@@ -1,4 +1,26 @@
 package anything.leetcode.mid;
 
 public class Problem14 {
+  class Solution {
+    public int kthSmallest(TreeNode root, int k) {
+        
+        int leftSize = treeSize(root.left);
+        if(k == leftSize + 1){
+            return root.val;
+        }else if(leftSize+1  >  k){
+            return kthSmallest(root.left,k);
+        }else{
+            return kthSmallest(root.right,k-leftSize-1);
+        }
+    }
+    
+    private int treeSize(TreeNode root){
+        if(root == null){
+            return 0;
+        }
+        return 1 + treeSize(root.left) + treeSize(root.right);
+    }
+    
+}
+  
 }
